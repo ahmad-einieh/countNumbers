bc8c25b602ad975e7d509ceac7b0673bc4eae184
Ahmad Einieh <66053897+ahmad-einieh@users.noreply.github.com>
2022-10-24 16:00:48 +0300

    new

--- src/Main.java
+++ src/Main.java
@@ -3,36 +3,34 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int a = 0;
-        int b =0;
-        int c=0;
-        do{
+        int b = 0;
+        int c = 0;
+        do {
             System.out.print("Enter a number: ");
             Scanner sc = new Scanner(System.in);
             int temp = sc.nextInt();
-            if(temp>0){
+            if (temp > 0) {
                 a++;
-            }
-            else if(temp<0){
+            } else if (temp < 0) {
                 b++;
-            }
-            else{
+            } else {
                 c++;
             }
             System.out.print("Do you want to continue? (y/n): ");
             Scanner sc1 = new Scanner(System.in);
             String temp1 = sc1.next();
-            if (temp1.equals("n")){
+            if (temp1.equals("n")) {
                 break;
-            }else if(temp1.equals("y")){
+            } else if (temp1.equals("y")) {
                 continue;
-            }else {
+            } else {
                 System.out.println("Invalid input");
             }
-        }while (true);
+        } while (true);
 
-        System.out.println("Positive numbers: "+a);
-        System.out.println("Negative numbers: "+b);
-        System.out.println("Zeroes: "+c);
+        System.out.println("Positive numbers: " + a);
+        System.out.println("Negative numbers: " + b);
+        System.out.println("Zeroes: " + c);
 
     }
 }
